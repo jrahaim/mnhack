87f2fae1ae57767239a097080997dfb9d077ca2a
Jason Rahaim <jason@jasonr.com>
2020-06-16 15:01:32 -0400

    Initial Commit

---
+++ mnhack/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  mnhack
+//
+//  Created by Jason Rahaim on 6/16/20.
+//  Copyright © 2020 jrdev. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
